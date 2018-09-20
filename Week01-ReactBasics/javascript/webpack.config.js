bbc04dd784b538bb715ace21020b2bf7d9befd8d
Hannah Pittman <hannah.pittman@bellevuecollege.edu>
2018-09-19 19:16:16 -0700

    linked in stateless code

---
+++ Week01-ReactBasics/javascript/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './main.js',
+    output: {path: __dirname, filename: 'bundle.js'},
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['env', 'react']
+                }
+            }
+        ]
+    },
+};
