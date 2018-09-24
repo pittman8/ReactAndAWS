ec83844405bfc611f3f6cbe18f35b74080d8c1bf
Hannah Pittman <hannah.pittman@bellevuecollege.edu>
2018-09-24 15:30:37 -0700

    changed to week02

--- Week01-ReactBasics/src/main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ReactBasics } from './ReactBasicsStatelessFunctional.js';
- 
-ReactDOM.render(<ReactBasics/>, document.getElementById('root'));
