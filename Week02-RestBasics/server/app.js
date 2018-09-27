1a3a1851d940d249fa8e4366649891579c9c7dd7
Hannah Pittman <hannah.pittman@bellevuecollege.edu>
2018-09-26 20:11:26 -0700

    initial commit

--- Week02-RestBasics/server/app.js
+++ Week02-RestBasics/server/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var api = require('./routes/api');
 var app = express();
 
 // view engine setup
@@ -24,7 +25,8 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) { 'use strict';
-  next(createError(404));
+    app.use('/api', api);
+    next(createError(404));
 });
 
 // error handler
