c4433914ab956514b538494bc5c7b550b8e840ae
Hannah Pittman <hannah.pittman@bellevuecollege.edu>
2018-10-17 16:18:43 -0700

    ec2 copy file in class

---
+++ week05-ec2-copy-file/client/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+    copyFile = () => {
+        const that = this;
+        fetch('/script-pusher/copy-file')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+                that.setState(foo => (json));
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+
+  render() {
+    return (
+        <div className="App">
+            <header>
+                <h1>Copy File</h1>
+            </header>
+            <main>
+                <button onClick={this.copyFile}>Copy File</button>
+            </main>
+        </div>
+    );
+  }
+}
+
+export default App;
