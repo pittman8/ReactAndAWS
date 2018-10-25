fadce14c5ede489a30a7cd2807c5e49263dbd35e
Hannah Pittman <hannah.pittman@bellevuecollege.edu>
2018-10-24 17:20:49 -0700

    worked in class

--- week06-system-check/client/src/App.js
+++ week06-system-check/client/src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            allData: 'unknown'
+        }
+    }
+
+    copyFile = () => {
+        const that = this;
+        fetch('/script-pusher/copy-file')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json.allData);
+                that.setState({allData: json.allData});
+            })
+            .catch(function(ex) {
+                console.log(
+                    'parsing failed, URL bad, network down, or similar',
+                    ex
+                );
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1>System Check</h1>
+                </header>
+                <main>
+                    <button onClick={this.copyFile}>Copy File</button>
+                    <pre>{this.state.allData}</pre>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
