7f6acb135b2eb620cbcd1da8ce3673d162bb2f86
Hannah Pittman <hannah.pittman@bellevuecollege.edu>
2018-10-31 17:10:07 -0700

    worked in class

--- week07-system-check-refactor/server/app.js
+++ week07-system-check-refactor/server/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var scriptPusher = require('./routes/script-pusher');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/script-pusher', scriptPusher);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,7 +31,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
     'use strict';
     // set locals, only providing error in development
     res.locals.message = err.message;
