786da10f4509a39d7677b9a899ac679e78a5c2e1
Hannah Pittman <hannah.pittman@bellevuecollege.edu>
2018-11-11 10:26:52 -0800

    no longer hard coding IP and Private Key for week06-run-ssh & midterm on branch week09 with tag v3.0.12.

--- midterm/server/routes/ssh-runner.js
+++ midterm/server/routes/ssh-runner.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var router = express.Router();
 const Client = require('ssh2').Client;
-
+const elfUtils = require('elven-code').elfUtils;
 const hostAddress = '18.235.68.201';
 
 let allData = '';
 
-const uptime = (hostAddress, response) => {
+const getSshIp = () => {
+    return new Promise(function (resolve, reject) {
+        elfUtils.readFile(process.env.HOME + '/.ssh/config')
+            .then((content) => {
+                var pattern = new RegExp('Host ec2-bc[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)');
+
+                const result = {};
+                const match = content.result.match(pattern);
+                for (let i = 1; i < 5; i++) {
+                    if (match[i].startsWith('HostName')) {
+                        var hostPattern = new RegExp('HostName\\s(.*)');
+                        result.hostName = match[i].match(hostPattern)[1];
+                    }
+                    if (match[i].startsWith('IdentityFile')) {
+                        const idPattern = new RegExp('IdentityFile\\s(.*)');
+                        const path = match[i].match(idPattern)[1];
+                        result.identityFile = path.substring(path.lastIndexOf('/') + 1, path.length)
+                    }
+                }
+                resolve(result);
+            })
+            .catch(reject);
+    });
+};
+
+const uptime = (hostName, identityFile, response) => {
     var conn = new Client();
     conn.on('ready', function() {
         console.log('Client :: ready');
@@ -33,18 +58,25 @@ const uptime = (hostAddress, response) => {
                 });
         });
     }).connect({
-        host: hostAddress,
+        host: hostName,
         port: 22,
         username: 'ubuntu',
         privateKey: require('fs').readFileSync(
-            process.env.HOME + '/.ssh/ec2-320-inclass.pem'
+            process.env.HOME + '/.ssh/' + identityFile
         )
     });
 };
 
 router.get('/uptime', function(request, response) {
-    'use strict';
-    uptime(hostAddress, response);
+    console.log('run-get-started called in ssh-runner', hostAddress);
+    getSshIp()
+        .then((result) => {
+            uptime(result.hostName, result.identityFile, response);
+        })
+        .catch((err) => {
+            response.send(err);
+        });
+
 });
 
 module.exports = router;
