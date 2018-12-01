8efa01afae46f19b632a40f02222552bc2fe9256
Hannah Pittman <hannah.pittman@bellevuecollege.edu>
2018-11-30 21:34:01 -0800

    system check ready for EC2

---
+++ week06-system-check/client/src/ElfHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class ElfHeader extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>System Check - EC2</h1>
+            </div>
+        );
+    }
+}
+export default ElfHeader;
--- week06-system-check/server/routes/script-pusher.js
+++ week06-system-check/server/routes/script-pusher.js
@@ -5,7 +5,28 @@ const spawn = require('child_process').spawn;
 
 let allData = '';
 
-const copyFile = () => {
+const check = (request, response, next) => {
+    console.log('REQUEST CHECK CALLED', request.query);
+    const validOptions = ['CpuInfo', 'VersionCheck', 'uptime'];
+    if (request.query.script) {
+        console.log('INSIDE REQUEST SCRIPT');
+        if (!validOptions.includes(request.query.script)) {
+            console.log('INSIDE REQUEST INVALID OPTION');
+            response.send({
+                result: 'error',
+                error: 'Invalid Option: ' + request.query.script,
+                script: request.query.script
+            });
+            return;
+        }
+    }
+    next();
+};
+
+router.use(check);
+
+
+const copyCpuInfo = () => {
     return new Promise(function(resolve, reject) {
         console.log('Run CPU Info', process.env.SETUP_LINUXBOX);
 
@@ -40,9 +61,72 @@ const copyFile = () => {
     });
 };
 
+const scriptRunner = (path, script) => {
+    allData = '';
+    return new Promise(function(resolve, reject) {
+        console.log('Run CPU Info', path);
+
+        const pushScript = spawn(path + '/' + script);
+
+        pushScript.stdout.on('data', data => {
+            console.log(`child stdout:\n${data}`);
+            allData += data;
+            //console.log('PUSH', data);
+        });
+
+        pushScript.stderr.on('data', data => {
+            console.log(`child stderr:\n${data}`);
+            allData += data;
+            //console.error('PUSH', data);
+        });
+
+        pushScript.on('close', code => {
+            resolve({
+                result: 'success',
+                allData: allData,
+                code: code
+            });
+        });
+
+        pushScript.on('error', code => {
+            reject({
+                result: 'error',
+                code: code
+            });
+        });
+    });
+};
+
 router.get('/copy-file', function(request, response) {
     'use strict';
-    copyFile()
+    copyCpuInfo()
+        .then(result => {
+            console.log(JSON.stringify(result, null, 4));
+            response.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+            response.send(err);
+        });
+});
+
+router.get('/run-script', function(request, response) {
+    'use strict';
+    scriptRunner(process.env.SETUP_LINUXBOX, request.query.script)
+        .then(result => {
+            console.log(JSON.stringify(result, null, 4));
+            response.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+            response.send(err);
+        });
+});
+
+router.get('/run-system-tool', function(request, response) {
+    'use strict';
+    allData = '';
+    scriptRunner('/usr/bin/', request.query.script)
         .then(result => {
             console.log(JSON.stringify(result, null, 4));
             response.send(result);
